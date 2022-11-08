d7409b2b08e77170cf1e84ccb567d487477aafbe
Jakarin Chujan <jakarin.chujan@gmail.com>
2022-11-08 16:44:20 +0700

    :art: Reformat the file

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
-#from typing import List
-#from uuid import UUID
+# from typing import List
+# from uuid import UUID
 from fastapi import FastAPI, HTTPException
-#from pymongo import MongoClient
 
-#from models.models import Gender, Role, User
+# from pymongo import MongoClient
+
+# from models.models import Gender, Role, User
 
 app = FastAPI()
 
-#client = MongoClient("mongodb://localhost:27017/")
-#db = client["GovernmentCatnip"]
 
-#db: List[User] = [
+# client = MongoClient("mongodb://localhost:27017/")
+# db = client["GovernmentCatnip"]
+
+# db: List[User] = [
 #    User(
 #        id="31b6e462-4c9a-4d3d-a00a-e7a8fc43c4e7", 
 #        first_name="Jamila", 
@@ -25,7 +27,7 @@ app = FastAPI()
 #        gender=Gender.male,
 #        roles=[Role.admin, Role.user]
 #    )
-#]
+# ]
 
 @app.get("/")
 async def root():
@@ -33,18 +35,18 @@ async def root():
         "message": "Sup for our documentation go to link variable",
         "link": "https://catnip-govenment-module.github.io/government-catnip"
     }
-    
-#@app.get("/api/v1/users")
-#async def fetch_users():
+
+# @app.get("/api/v1/users")
+# async def fetch_users():
 #    return db;
 #
-#@app.post("/api/v1/users")
-#async def register_user(user: User):
+# @app.post("/api/v1/users")
+# async def register_user(user: User):
 #    db.append(user)
 #    return {"id": user.id}
 #
-#@app.delete("/api/v1/users/{user_id}")
-#async def delete_user(user_id: UUID):
+# @app.delete("/api/v1/users/{user_id}")
+# async def delete_user(user_id: UUID):
 #        for user in db:
 #            if user.id == user_id:
 #                db.remove(user)
