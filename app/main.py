51f7f0a338058194c5d15731b47b92144176e3fe
Jakarin-Jojo <jakarin.chujan@gmail.com>
2022-11-12 20:06:52 +0700

    :coffin:Delete unused code

--- app/main.py
+++ app/main.py
@@ -5,31 +5,12 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pymongo import MongoClient
 
-# from models.models import Gender, Role, User
 
 app = FastAPI()
 
 client = MongoClient(host="db")
 db = client["government_catnip"]
 dbLocations = db["location_information"]
-
-
-# db: List[User] = [
-#    User(
-#        id="31b6e462-4c9a-4d3d-a00a-e7a8fc43c4e7", 
-#        first_name="Jamila", 
-#        last_name="Ahmed",
-#        gender=Gender.female,
-#        roles=[Role.student]
-#    ),
-#    User(
-#        id="03dfb08b-faa6-47fc-b60c-3484a62f8b2a", 
-#        first_name="Alex", 
-#        last_name="Jones",
-#        gender=Gender.male,
-#        roles=[Role.admin, Role.user]
-#    )
-# ]
 
 
 @app.get("/")
@@ -45,25 +26,3 @@ def locations():
     location = dbLocations.find()
     list_location = [l for l in location]
     return list_location
-
-
-# @app.get("/api/v1/users")
-# async def fetch_users():
-#    return db;
-#
-# @app.post("/api/v1/users")
-# async def register_user(user: User):
-#    db.append(user)
-#    return {"id": user.id}
-#
-# @app.delete("/api/v1/users/{user_id}")
-# async def delete_user(user_id: UUID):
-#        for user in db:
-#            if user.id == user_id:
-#                db.remove(user)
-#                return
-#        raise HTTPException(
-#            status_code=404,
-#            detail=f"user with id: {user_id} does not exists"
-#        )
-#
