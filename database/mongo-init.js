ee9cadc371043d38e4e27bcc34443b1c44fd4a0a
Jakarin-Jojo <jakarin.chujan@gmail.com>
2022-11-12 14:54:13 +0700

    :card_file_box: Update db

--- database/mongo-init.js
+++ database/mongo-init.js
@@ -8,7 +8,7 @@ db.createCollection("district")
 
 db.personal_information.insertMany([
     {
-        "_id": 4569871354123,
+        "citizen_id": 4569871354123,
         "title": "Mr.",
         "firstName": "Anuman",
         "lastName": "Saengthong",
@@ -18,7 +18,7 @@ db.personal_information.insertMany([
         "blackList": false
     },
     {
-        "_id": 1234567898765,
+        "citizen_id": 1234567898765,
         "title": "Ms.",
         "firstName": "Achara",
         "lastName": "Sukkasem",
@@ -31,24 +31,24 @@ db.personal_information.insertMany([
 
 db.personal_cvv.insertMany([
     {
-        "_id": 4569871354123,
+        "citizen_id": 4569871354123,
         "cvv": "267d303819c7fa1c19716d44316e8a1d63f4a76fdeca8aa337d5215d60dca0f3"
     },
     {
-        "_id": 1234567898765,
+        "citizen_id": 1234567898765,
         "cvv": "e192ab76c7830b54f5d70e09ed920511f5f2f1afc468f5379ded05fdd71ab6a5"
     }
 ])
 
 db.location_information.insertMany([
     {
-        "_id": 1, // ID of location
+        "location_id": 1, // ID of location
         "location": "Amphawa", // Location
         "population": 10000, // Population in this location
         "numberOfVoters": 9995 // Amount of who have right to vote
     },
     {
-        "_id": 2,
+        "location_id": 2,
         "location": "Bang Len",
         "population": 20000,
         "numberOfVoters": 18995
@@ -57,7 +57,7 @@ db.location_information.insertMany([
 
 db.district.insertMany([
     {
-        "_id": 1,
+        "district_id": 1,
         "district": "Amphawa",
         "districtTH": "อัมพวา",
         "province": "Samut Songkhram",
@@ -65,7 +65,7 @@ db.district.insertMany([
         "region": "Centre"
     },
     {
-        "_id": 2,
+        "district_id": 2,
         "district": "Bang Len",
         "districtTH": "บางเลน",
         "province": "Nakhon Pathom",
