04b09aea70b33c18875fd9999012df068ba28497
Wha Chanunya <whachanunya@gmail.com>
2022-11-12 22:10:04 +0700

    create election result model

--- app/main.py
+++ app/main.py
@@ -1,31 +1,14 @@
-#from typing import List
-#from uuid import UUID
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from pymongo import MongoClient
-
-#from models.models import Gender, Role, User
+from typing import List
+from pydantic import BaseModel
+import collections
 
 app = FastAPI()
 
-client = MongoClient(host="db")
+client = MongoClient("db")
 db = client["government_catnip"]
-
-#db: List[User] = [
-#    User(
-#        id="31b6e462-4c9a-4d3d-a00a-e7a8fc43c4e7", 
-#        first_name="Jamila", 
-#        last_name="Ahmed",
-#        gender=Gender.female,
-#        roles=[Role.student]
-#    ),
-#    User(
-#        id="03dfb08b-faa6-47fc-b60c-3484a62f8b2a", 
-#        first_name="Alex", 
-#        last_name="Jones",
-#        gender=Gender.male,
-#        roles=[Role.admin, Role.user]
-#    )
-#]
+db_election_result = db["election_result"]
 
 @app.get("/")
 async def root():
@@ -33,24 +16,20 @@ async def root():
         "message": "Sup for our documentation go to link variable",
         "link": "https://catnip-govenment-module.github.io/government-catnip"
     }
-    
-#@app.get("/api/v1/users")
-#async def fetch_users():
-#    return db;
-#
-#@app.post("/api/v1/users")
-#async def register_user(user: User):
-#    db.append(user)
-#    return {"id": user.id}
-#
-#@app.delete("/api/v1/users/{user_id}")
-#async def delete_user(user_id: UUID):
-#        for user in db:
-#            if user.id == user_id:
-#                db.remove(user)
-#                return
-#        raise HTTPException(
-#            status_code=404,
-#            detail=f"user with id: {user_id} does not exists"
-#        )
-#
+
+
+class ElectionResult(BaseModel):
+    location_id: int
+    location: str
+    numberOfVotes: int
+    nameOfParliament: str 
+    nameOfParty: str
+
+# @app.post("/api/v1/election-result")
+# async def create_election_result(result: ElectionResult):
+#     db_election_result = {}
+#     location_id = result.location_id
+#     db_election_result[location_id] = result.dict()
+#     return {
+#         "status": 200
+#         }
