92fd764e3c2e48974b45a9f88fb433fc8ff551c9
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2022-11-14 16:37:17 +0700

    change test code

--- tests/test_root.py
+++ tests/test_root.py
@@ -1,15 +1,7 @@
-from fastapi.testclient import TestClient
-
-from app.main import app
-
-client = TestClient(app)
-
-
-def test_main(test_app):
+def test_main(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {
         "message": "Sup for our documentation go to link variable",
         "link": "https://catnip-govenment-module.github.io/government-catnip"
     }
-    
