208fd5fcf444bdd93f43fa0930c16113af20c73b
Jakarin-Jojo <jakarin.chujan@gmail.com>
2022-11-14 16:32:50 +0700

    :wrench:Modify MongoDB connections to connect via getdb() function.:wrench:

--- app/main.py
+++ app/main.py
@@ -1,22 +1,27 @@
 from typing import List
 # from uuid import UUID
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 from pydantic import BaseModel
 from pymongo import MongoClient
+from pymongo.database import Database
 
 app = FastAPI()
 
-client = MongoClient(host="db")
-db = client["government_catnip"]
-dbLocations = db["location_information"]
+
+async def get_db():
+    client = await check_connect_mongodb()
+    db = client["government_catnip"]
+    return db
 
 
-async def check_connection_mongodb():
+async def check_connect_mongodb():
     try:
+        client = MongoClient(host="db")
         client.server_info()
     except Exception:
         raise HTTPException(status_code=500, detail=f"Unable to connect to the server")
+    return client
 
 
 @app.get("/")
@@ -28,8 +33,9 @@ async def root():
 
 
 @app.get("/api/v1/locations", summary="Return all location with detail")
-async def locations():
-    await check_connection_mongodb()
+async def locations(db: Database = Depends(get_db)):
+    # await check_connection_mongodb()
+    dbLocations = db["location_information"]
     location = dbLocations.find()
     list_location = [l for l in location]
     if list_location:
