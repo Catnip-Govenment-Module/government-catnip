aa2c781a3c0221d095c52b6c3256c8eff823e1a7
Jakarin-Jojo <jakarin.chujan@gmail.com>
2022-11-14 18:30:52 +0700

    :white_check_mark:Update test get_location:white_check_mark:

--- tests/test_get_location.py
+++ tests/test_get_location.py
@@ -1,13 +1,13 @@
 def test_get_location(mock_mongo, client, db_location):
     test_get_all_location = [
         {
-            "_id": 1,
+            "location_id": 1,
             "location": "Amphawa",
             "population": 10000,
             "numberOfVoters": 9995
         },
         {
-            "_id": 2,
+            "location_id": 2,
             "location": "Bang Len",
             "population": 20000,
             "numberOfVoters": 18995
@@ -16,7 +16,20 @@ def test_get_location(mock_mongo, client, db_location):
     db_location.insert_many(test_get_all_location)
     response = client.get("/api/v1/locations")
     assert response.status_code == 200
-    assert response.json() == test_get_all_location
+    assert response.json() == [
+        {
+            "location_id": 1,
+            "location": "Amphawa",
+            "population": 10000,
+            "numberOfVoters": 9995
+        },
+        {
+            "location_id": 2,
+            "location": "Bang Len",
+            "population": 20000,
+            "numberOfVoters": 18995
+        }
+    ]
 
 
 def test_get_all_location_but_not_data(mock_mongo, client, db_location):
@@ -25,5 +38,3 @@ def test_get_all_location_but_not_data(mock_mongo, client, db_location):
     response = client.get("/api/v1/locations")
     assert response.status_code == 404
     assert response.json() == test_get_no_data
-
-
