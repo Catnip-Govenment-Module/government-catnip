a8ded984729fa751e84e03c96091c84e285d4a2e
Jakarin-Jojo <jakarin.chujan@gmail.com>
2022-11-14 16:54:10 +0700

    :clown_face:Mock mongoDB for test:clown_face:

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,25 @@
+import mongomock
 import pytest
 from fastapi.testclient import TestClient
-from app.main import app
+from app.main import app, get_db
+
+mongo_client = mongomock.MongoClient()
+db = mongo_client["government_catnip"]
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
+def mock_mongo():
+    def fake_db():
+        return db
+
+    app.dependency_overrides[get_db] = fake_db
+
+
+@pytest.fixture()
+def db_location():
+    yield db["location_information"]
+
+
+@pytest.fixture()
 def client():
     yield TestClient(app)
