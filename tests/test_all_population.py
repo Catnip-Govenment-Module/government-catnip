d45d659ac7ec55c4f2e02dc6bf1ea560c52bf7bb
Jakarin-Jojo <jakarin.chujan@gmail.com>
2022-11-16 03:15:40 +0700

    :white_check_mark:Update test GET population:white_check_mark:

--- tests/test_all_population.py
+++ tests/test_all_population.py
@@ -23,17 +23,9 @@ def test_get_population_endpoint(mock_mongo, mock_population, client):
     ]
 
 
-# def test_get_all_population_but_not_data(mock_mongo, client, db_population):
-#     db_population.delete_many({})
-#     test_get_population_no_data = {"detail": "No data"}
-#     response = client.get("/api/v1/populations")
-#     assert response.status_code == 404
-#     assert response.json() == test_get_population_no_data
-    
-def test_get_invalid_part(mock_mongo, client):
-    response = client.get("/api/v1/meow")
+def test_get_all_population_but_not_data(mock_mongo, client, db_population):
+    db_population.delete_many({})
+    test_get_population_no_data = {"detail": "No data"}
+    response = client.get("/api/v1/populations")
     assert response.status_code == 404
-
-# def test_post_election_result(mock_mongo, client):
-#     response = client.get("/api/v1/populations" )
-#     assert response.status_code == 200
+    assert response.json() == test_get_population_no_data
