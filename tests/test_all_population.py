e1d49ae2ad8f0694f2f0c655c926ed82a97fd935
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2022-11-15 23:24:04 +0700

    merge

--- tests/test_all_population.py
+++ tests/test_all_population.py
@@ -1,8 +1,33 @@
-def test_population_endpoint(client):
+def test_get_population_endpoint(client):
     response = client.get("/api/v1/populations")
     assert response.status_code == 200
+    assert response.json() == [
+   {
+        "_id": 4569871354123,
+        "title": "Mr.",
+        "firstName": "Anuman",
+        "lastName": "Saengthong",
+        "sex": "Male",
+        "locationID": 557,
+        "rightToVote": True,
+        "blackList": False
+    },
+    {
+        "_id": 1234567898765,
+        "title": "Ms.",
+        "firstName": "Achara",
+        "lastName": "Sukkasem",
+        "sex": "Female",
+        "locationID": 231,
+        "rightToVote": True,
+        "blackList": False
+    }
+    ]
 
-def test_invalid_id1(client):
-    response = client.get("/fruit/99")
+
+def test_get_all_population_but_not_data(client, db_population):
+    db_population.delete_many({})
+    test_get_population_no_data = {"detail": "No data"}
+    response = client.get("/api/v1/populations")
     assert response.status_code == 404
-    
+    assert response.json() == test_get_population_no_data
