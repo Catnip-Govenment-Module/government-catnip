232b78ed969d2ecdf2550e81190153d4acb3cabd
Wha Chanunya <whachanunya@gmail.com>
2022-11-15 21:43:16 +0700

    add mock_mock to parameter

--- tests/test_election_result_ec.py
+++ tests/test_election_result_ec.py
@@ -1,11 +1,11 @@
-def test_post_json_with_wrong_format(client):
+def test_post_json_with_wrong_format(mock_mongo, client):
      response = client.post(
          "/api/v1/election-results",
          json={"id": "bazz", "title": "Bazz", "description": "Drop the bazz"},
      )
      assert response.status_code == 422
 
-def test_post_json(client):
+def test_post_json(mock_mongo, client):
     response = client.post(
         "/api/v1/election-results", json={
             "location_id": 1,
@@ -18,11 +18,11 @@ def test_post_json(client):
     )
     assert response.status_code == 422
 
-def test_post_form_no_body(client):
+def test_post_form_no_body(mock_mongo, client):
     response = client.post("/api/v1/election-results")
     assert response.status_code == 422
 
-def test_post_election_result(client):
+def test_post_election_result(mock_mongo, client):
     data = [
             {
                 "location_id": 1,
@@ -46,7 +46,7 @@ def test_post_election_result(client):
     )
     assert response.status_code == 200
 
-def test_post_empty_list(client):
+def test_post_empty_list(mock_mongo, client):
     response = client.post(
         "/api/v1/election-results",
         json=[]
