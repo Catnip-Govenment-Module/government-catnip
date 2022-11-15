40e0ac2566228d987a8bff3f515459b2b8c407c3
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2022-11-16 00:14:31 +0700

    change parameter and some code

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from pymongo.database import Database
 import collections
 
 from app.models.location import Location
-from app.models.election_result import ElectionResult
+from app.models.population import Population
 
 app = FastAPI()
 
@@ -41,11 +41,13 @@ async def locations(db: Database = Depends(get_db)):
         return list_location
     raise HTTPException(status_code=404, detail="No data")
 
-@app.get("/api/v1/populations",description='populations information')
-async def all_pop():
+@app.get("/api/v1/populations",description='populations information', response_model=List[Population])
+async def all_population_info(db: Database = Depends(get_db)):
     try:
-        population = db_populations.find()
-        list_population = [l for l in population]
-        return list_population
+        dbpopulation = db["personal_information"]
+        populations = dbpopulation.find()
+        list_population = [l for l in populations]
+        if list_population:
+            return list_population
     except:
-        raise HTTPException(status_code=422)
+        raise HTTPException(status_code=404, detail="No data")
