07d6fca685a045764f4e2c3dd0c0f59ebe1d2349
Kodchakan Prajob <88821578+jaybjackie@users.noreply.github.com>
2022-11-18 10:34:28 +0700

    Change test to GET method

--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,9 +1,9 @@
 def test_check_validate(client):
     """Correct params"""
-    response = client.post("/validate_cvv/9876543210123/456")
+    response = client.get("/validate_cvv/9876543210123/456")
     assert response.status_code == 200
 
 def test_check_validate(client):
     """Correct params"""
-    response = client.post("/validate_cvv/9876543210123/123")
+    response = client.get("/validate_cvv/9876543210123/123")
     assert response.status_code == 404
